c7397dd150cca1304ecb34ee8f8e263f854a4c72
v3rbat1m <k.krivicius@gmail.com>
2020-09-08 12:15:30 +0300

    Updates

---
+++ precache-manifest.b84a78cbb45f7753cc1657296db1872b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f0504ad47ca5153b89dc7ddd60eb30",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "9ac232e48bff68a09c84",
+    "url": "/my-website/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "26c117a64066d94f2fcf",
+    "url": "/my-website/static/css/main.12e70d5b.chunk.css"
+  },
+  {
+    "revision": "9ac232e48bff68a09c84",
+    "url": "/my-website/static/js/2.76b88c93.chunk.js"
+  },
+  {
+    "revision": "ce1ec8d1775b6cacfcf398a3ab55b6dd",
+    "url": "/my-website/static/js/2.76b88c93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c117a64066d94f2fcf",
+    "url": "/my-website/static/js/main.d1242938.chunk.js"
+  },
+  {
+    "revision": "f6554984e335066966fc",
+    "url": "/my-website/static/js/runtime-main.3a9c6381.js"
+  },
+  {
+    "revision": "1667426cd9b01e0ae1b6ed2f4948d536",
+    "url": "/my-website/static/media/background.1667426c.jpg"
+  },
+  {
+    "revision": "3ce4dffb9a099249bc99375b70a2ca22",
+    "url": "/my-website/static/media/myPhoto.3ce4dffb.jpg"
+  }
+]);
